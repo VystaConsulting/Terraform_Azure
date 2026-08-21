5bc8a237bc61ce3d686804684f318f294feeac30
Yves Van Stappen <3679299+YvesVanStappen@users.noreply.github.com>
2026-08-21 15:39:42 +0200

    chore: add a comment

--- src/azure-storage-terraform/main.tf
+++ src/azure-storage-terraform/main.tf
@@ -3,11 +3,12 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
+# Comment
 resource "azurerm_storage_account" "example" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
-  account_tier            = var.sku
+  account_tier             = var.sku
   account_replication_type = "LRS"
 
   tags = {
